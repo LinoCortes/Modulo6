466f67bb7abb94b547f338c27e459d51333ee60e
Lino Cortés <lino.cortess@gmail.com>
2023-06-27 18:30:45 -0400

    EjercicioGrupal1

---
+++ src/main/java/servlets/ServletIndex.java
@@ -0,0 +1,18 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletIndex
+ */
+public class ServletIndex extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Dentro del ServletIndex");
+	}
+}
